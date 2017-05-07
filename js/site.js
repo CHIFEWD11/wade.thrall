734044a09c12851fc2ea6fca4b1e2e49ab7f96a2
wadethrall <wadethrall@gmail.com>
2017-05-07 04:20:16 -0500

    changed to a simpler text container for each page
    next up is to get the "more" buttons to change to "less" and toggle so
    that the content will hide and show.
    Also need to get the pictures and video to be responsive

--- js/site.js
+++ js/site.js
@@ -24,72 +24,34 @@ $(".sidenav").on("mouseleave", function(){
  		closeNav();
 });
 
-$(".more").on("click", function(){
-	$(".textBox").css({height: 'auto', overflow: 'auto'});
+// $(".more").on("click", function(){
+// 	event.preventDefault();
+// 	$(".textBox").css({height: 'auto', overflow: 'auto'});
+	
+// });
+
+$("#homepage .more").on("click", function(){
 	event.preventDefault();
-})
+	$(".moreHomepage").show();
+	
+});
 
-function parallaxIt() {
-// create variables
-  	var $fwindow = $(window);
-   	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+$("#liturgical .more").on("click", function(){
+	event.preventDefault();
+	$(".moreLit").show();
+});
 
-   var $contents = [];
-   var $backgrounds = [];
+$("#rosary .more").on("click", function(){
+	event.preventDefault();
+	$(".moreRosary").show();
+});
 
-   // for each of content parallax element
-   $('[data-type="content"]').each(function(index, e) {
-     var $contentObj = $(this);
+$("#testimonial .more").on("click", function(){
+	event.preventDefault();
+	$(".moreTest").show();
+});
 
-   	 $contentObj.__speed = ($contentObj.data('speed') || 1);
-     $contentObj.__fgOffset = $contentObj.offset().top;
-     $contents.push($contentObj);
-   });
-
-   // for each of background parallax element
-   $('[data-type="background"]').each(function() {
-     var $backgroundObj = $(this);
-
-     $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
-     $backgroundObj.__fgOffset = $backgroundObj.offset().top;
-     $backgrounds.push($backgroundObj);
-   });
-
-   // update positions
-   $fwindow.on('scroll resize', function() {
-     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-     $contents.forEach(function($contentObj) {
-       var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed;
-
-       $contentObj.css('top', yPos);
-     })
-
-     $backgrounds.forEach(function($backgroundObj) {
-       var yPos = -((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
-
-       $backgroundObj.css({
-         backgroundPosition: '50% ' + yPos + 'px'
-       });
-     });
-   });
-
-   // triggers winodw scroll for refresh
-   $fwindow.trigger('scroll');
- };
-
-parallaxIt();
-// /*
-// // $(window).on('scroll', function() {
-// //     var scrollTop = $(this).scrollTop();
-
-// //     $('#homepage, #liturgical').each(function() {
-// //         var topDistance = $(this).offset().top;
-
-// //         if ( (topDistance+100) < scrollTop ) {
-// //             alert( $(this).text() + ' was scrolled to the top' );
-// //         }
-// //     });
-// // });
-// */
-
+$("#michele .more").on("click", function(){
+	event.preventDefault();
+	$(".moreMichele").show();
+});
